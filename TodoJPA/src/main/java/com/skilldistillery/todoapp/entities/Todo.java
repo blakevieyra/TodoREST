1d32abfaf79a9911d65b0a15861731908ff55051
Blake Vieyra <blake.vieyra@gmail.com>
2024-03-04 20:54:14 -0800

    pushing project

---
+++ TodoJPA/src/main/java/com/skilldistillery/todoapp/entities/Todo.java
@@ -0,0 +1,148 @@
+package com.skilldistillery.todoapp.entities;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Todo {
+
+	public Todo() {
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String task;
+
+	private String description;
+
+	private boolean completed;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@CreationTimestamp
+	@Column(name = "due_date")
+	private LocalDateTime dueDate;
+
+	@CreationTimestamp
+	@Column(name = "complete_date")
+	private LocalDateTime completedDate;
+
+	@CreationTimestamp
+	@Column(name = "created_at")
+	private LocalDateTime createdAt;
+
+	@UpdateTimestamp
+	@Column(name = "updated_at")
+	private LocalDateTime updatedAt;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTask() {
+		return task;
+	}
+
+	public void setTask(String task) {
+		this.task = task;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public boolean isCompleted() {
+		return completed;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public LocalDateTime getCompletedDate() {
+		return completedDate;
+	}
+
+	public void setCompletedDate(LocalDateTime completedDate) {
+		this.completedDate = completedDate;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public LocalDateTime getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(LocalDateTime dueDate) {
+		this.dueDate = dueDate;
+	}
+
+	public void setCompleted(boolean completed) {
+		this.completed = completed;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Todo other = (Todo) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "Todo [id=" + id + ", task=" + task + ", description=" + description + ", completed=" + completed
+				+ ", user=" + user + ", dueDate=" + dueDate + ", completedDate=" + completedDate + ", createdAt="
+				+ createdAt + ", updatedAt=" + updatedAt + "]";
+	}
+}
